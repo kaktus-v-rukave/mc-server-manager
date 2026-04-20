88a62a2a06df6e8733379579ad20db124c5d3e91
KakTuS <pro100k1ryuha@gmail.com>
2026-04-20 22:23:19 +0500

    decorator "admin_only"

--- handlers/routes.py
+++ handlers/routes.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from os import getenv
 from dotenv import load_dotenv
+from functools import wraps
 
 load_dotenv()
 
@@ -15,6 +16,18 @@ def is_admin(uid):
 def is_owner(uid):
     owner_id = getenv('OWNER_ID')
     return str(uid) == owner_id
+
+def admin_only(handler):
+    @wraps(handler)
+    async def wrapper(message: Message, *args, **kwargs):
+        admins_id = set(getenv('ADMINS_ID').split(';'))
+        owner_id = getenv('OWNER_ID')
+        uid = str(message.from_user.id)
+
+        if uid in admins_id or uid == owner_id:
+            return await handler(message, *args, **kwargs)
+        return await message.answer('В доступе отказано.')
+    return wrapper
 
 def admin_panel():
     keyboard = ReplyKeyboardMarkup(
@@ -41,6 +54,7 @@ async def get_id(message: Message):
 
 
 @router.message(Command('mc_server_op'))
+@admin_only
 async def mc_server_op(message: Message):
     if is_admin(message.from_user.id):
         await message.answer(
