3dd568ba96caf0fafaa82e9db0c7d723d8d1ab26
KakTuS <pro100k1ryuha@gmail.com>
2026-04-25 23:06:58 +0500

    status button removed

--- keyboards/reply_keyboards.py
+++ keyboards/reply_keyboards.py
@@ -4,7 +4,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 def server_menu():
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text='Перезапуск'), KeyboardButton(text='Состояние')],
+            [KeyboardButton(text='Перезапуск')],
             [KeyboardButton(text='Пуск'), KeyboardButton(text='Стоп')]
         ],
         resize_keyboard=True
