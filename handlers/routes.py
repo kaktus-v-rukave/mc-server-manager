ac33a587bd0662b954dcd07ca306d55d559ad45a
KakTuS <pro100k1ryuha@gmail.com>
2026-04-25 13:12:08 +0500

    start command timeout change

--- handlers/routes.py
+++ handlers/routes.py
@@ -62,5 +62,5 @@ async def stop_server(message: Message):
 async def start_server(message: Message):
     sent = await message.answer('<b>Запускаю</b> 🚀', parse_mode='HTML')
     subprocess.run(["screen", "-S", "mc", "-X", "stuff", "./run.sh\n"])
-    await asyncio.sleep(8)
+    await asyncio.sleep(15)
     await sent.edit_text('<b>Сервер запущен</b>', parse_mode='HTML')
